03514327cabe894bee9fdc8777fbff20cd786306
fredaugermorin <frederick.augermorin@gmail.com>
2016-08-23 17:08:30 -0400

    refined position and portfolio classes

--- src/Position.py
+++ src/Position.py
@@ -10,7 +10,11 @@ class Position:
         self.isin = _isin
         self.sedol = _sedol
         self.data = []
+        self.weight = 0
 
+    def setWeight(self,_weight):
+        self.weight = _weight
+        
     def display(self):
         myStr = '##############\n'
         myStr = myStr + 'Ticker: '+self.ticker+'\n'+\
@@ -28,43 +32,59 @@ class Position:
 
         print(myStr)
 
-    def marketValue(self):
-        return self.quantity * self.price
+    def costValue(self):
+        return float(self.quantity * self.price)
 
+    def marketValue(self,date):
+        return float(self.data.ix[date]['AdjustedClosePrice'])*self.quantity
+        
+    
     def getTimeSeries(self,con):
-        query = "Select distinct * from SecuritiesMaster.dailyprice where SecurityIdentifier = '" + self.ticker +\
+        fields = 'ValuationDate, OpenPrice, HighPrice, LowPrice, ClosePrice, AdjustedClosePrice,Volume'
+        query = "Select distinct "+ fields+" from SecuritiesMaster.dailyprice where SecurityIdentifier = '" + self.ticker +\
                 "' order by ValuationDate asc"
         with con.cursor()  as cursor:
             try:
                 cursor.execute(query)
                 self.data = pandas.DataFrame(cursor.fetchall())
+                self.data=self.data.set_index('ValuationDate')
             except:
                 print('Error retreiving data for: '+ self.ticker+' please make sure it is in MySQL!')
         cursor.close()
 
 
 class Portfolio:
-    def __init__(self, _portfolioId, _ptfAsOfDate, _positions=[], _weights=[]):
+    def __init__(self, _portfolioId, _ptfAsOfDate, _positions=[]):
         self.id = _portfolioId
         self.date = _ptfAsOfDate
         self.positions = _positions
-        self.weights = _weights
+        self.weight = 0
+
+    def costValue(self):
+        costVal = 0
+        for position in self.positions:
+            costVal += position.costValue()
+        return float(costVal)
 
     def marketValue(self):
         mktVal = 0
         for position in self.positions:
-            mktVal += position.marketValue()
-        return mktVal
-
+            mktVal += position.marketValue(self.date) 
+        return float(mktVal)
+            
     def computeWeights(self):
         mktVal = self.marketValue()
-        weights = {}
+        totalWeight = 0
         for pos in self.positions:
-            if pos.ticker not in weights.keys():    
-                weights[pos.ticker] = pos.marketValue() / mktVal
-            else:
-                weights[pos.ticker]+= pos.marketValue() / mktVal
-        self.weights = weights
+                pos.setWeight(pos.marketValue(self.date) / mktVal)
+                totalWeight += pos.weight
+        self.weight = float(totalWeight)
+
+    def PnL(self):
+        pnl = [0,0]
+        pnl[0] = self.marketValue() - self.costValue()
+        pnl[1] = (self.marketValue() / self.costValue() - 1)*100
+        return pnl
     
     def makePortfolio(self,con):
         features = 'PositionType, Quantity, Price, Currency, SecurityIdentifier, CUSIP, ISIN, SEDOL'
@@ -79,16 +99,23 @@ class Portfolio:
         #Parse all dataframe rows and create position object
         for row in myPositions.itertuples():
             thisPosition= Position(row[4],row[6],row[5],row[2],row[8],row[1],row[3],row[7])
+            thisPosition.getTimeSeries(con)
             self.positions.append(thisPosition)
         cursor.close()
         
     def display(self):
-        myStr = '********* Portfolio summary *********\n'
-        myStr+= '* Portfolio ID: '+self.id+ ' As of: '+self.date+ '*\n'
-        myStr+='********* Positions *********\n'
-        myStr+='* Identifier  |  Weight *\n'
-        for p in self.weights.keys():
-            myStr += '* '+p+' | '+str(self.weights[p]) +'*\n'
+        myStr = '************************* Portfolio summary ****************************\n'
+        myStr+= '* Portfolio ID: '+"%10s"%self.id+ '                          As of: '+self.date+ '  *\n'
+        myStr+= '***************************** Positions ********************************\n'
+        myStr+= '*      Identifier    | Weight | Cost Value | Market Value |     PnL $  *\n'
+        myStr+= '************************************************************************\n'
+        for p in self.positions:
+            myStr += '* '+"%19s"%p.ticker+'|'+"%8.2f"%p.weight+'|' +"%12.2f"%p.costValue()+'|' +"%14.2f"%p.marketValue(self.date)+\
+                     '|' +"%10.2f"%(p.marketValue(self.date)-float(p.costValue()))+'  *\n'
+        myStr += '************************************************************************\n'
+        myStr += '* '+"%19s"%('Total')+'|'+"%8.2f"%self.weight +'|' +"%12.2f"%self.costValue()+'|' +"%14.2f"%self.marketValue()
+        myStr += '|'+"%10.2f"%self.PnL()[0]+'  *\n'
+        myStr += '************************************************************************\n'
         print(myStr)
         
         
