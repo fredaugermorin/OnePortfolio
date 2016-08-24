c9259101076d831318298cadee38db088c1c8f97
fredaugermorin <frederick.augermorin@gmail.com>
2016-08-24 13:38:50 -0400

    modified bat file, only runs daily data download

--- src/scripts/FEED_DAILY_PRICE.bat
+++ src/scripts/FEED_DAILY_PRICE.bat
@@ -1,16 +1,9 @@
 @ECHO OFF
-SET /p action=Daily download(1) or batch(2):
 
-IF %action% == 1 (
-SET /p startDate=Enter daily download date yyyy-mm-dd :
-python "C:\Users\FRED\Desktop\OnePortfolio\src\scripts\feedDailyPrice.py" %startDate%
-)
-IF %action% == 2 (
-SET /p startDate=Enter download start date yyyy-mm-dd :
-SET /p endDate=Enter download end date yyyy-mm-dd :
-python "C:\Users\FRED\Desktop\OnePortfolio\src\scripts\feedDailyPrice.py" %startDate% %endDate%
-)
+SET /p startDate= "Enter daily download date yyyy-mm-dd :"
 
+cd C:\Users\FRED\Desktop\OnePortfolio\src\scripts\
+C:\Python34\python.exe feedDailyPrice.py %startDate%
 
 
 
